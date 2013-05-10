d9a967fb3b1fb3d3b50d5e29251cc2b690d401ff
cuonglee <cuongle1981@gmail.com>
2013-05-10 16:59:29 +0700

    initialize football pitch project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FPitch::Application.config.secret_token = '16acd49c2e043b7243077bb60aa273f000a1f2aed7b63c53adc2358548f2d2d59ec53cba8a079bcfdb410ef81fa49a20a06d40dea9537b01d310dbda662a44a8'
